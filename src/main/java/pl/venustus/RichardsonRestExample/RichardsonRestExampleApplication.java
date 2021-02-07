bc7f6f9f34ac72fd6297382d73db0774eaf285fb
venustuspl <venustus.pl@gmail.com>
2021-02-07 09:48:17 +0100

    first commit

---
+++ src/main/java/pl/venustus/RichardsonRestExample/RichardsonRestExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.RichardsonRestExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichardsonRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichardsonRestExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/venustus/RichardsonRestExample/RichardsonRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.RichardsonRestExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichardsonRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
